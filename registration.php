51dad523171a35fc5a3a5de23394805e95c53022
“rechchkernit” <rechchkernit@sqli.com>
2024-08-27 16:21:18 +0100

    Module standard structure

---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Chkernit_BackStyler extension
+ *
+ * @category   Chkernit
+ * @package    Chkernit_BackStyler
+ * @author     Chkernit Dev Team
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Chkernit_BackStyler',
+    __DIR__
+);
